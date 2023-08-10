b29222484419b3f598bd947f330229590cfb286c
Matt Graham <matthew.m.graham@gmail.com>
2023-08-10 14:42:27 +0100

    Change to PyData Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -39,5 +39,5 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
